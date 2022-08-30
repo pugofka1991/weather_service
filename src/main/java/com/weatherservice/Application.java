5187c92329dfc28f241dedcf1bbf83699b0fd5cd
SapsyrinaK <95683214+pugofka1991@users.noreply.github.com>
2022-08-30 12:55:06 +0300

    Update Application.java

--- src/main/java/com/weatherservice/Application.java
+++ src/main/java/com/weatherservice/Application.java
@@ -1,4 +1,4 @@
-package com.streletsa.weatherservice;
+package com.weatherservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
